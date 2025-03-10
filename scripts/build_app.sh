86ba3ef988d6d1cbb1c32bc0e3b8c04620fa9548
liamHowatt <liamjmh0@gmail.com>
2025-03-10 08:31:50 +0100

    copy from am62b-p1

---
+++ scripts/build_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /app/lv_port_linux
+
+cmake -B build-arm64 -S . \
+      -DCMAKE_CXX_FLAGS="-O3" \
+      -DCMAKE_C_FLAGS="-O3" \
+      -DCMAKE_C_FLAGS="-I/usr/include/libdrm" \
+      -DCMAKE_BUILD_TYPE=Release
+
+make -j -C build-arm64
